13b27098e726e23abb2a62cc5b2d66ab2a5d66e6
宣则宁 <hello@example.com>
2026-08-17 15:46:15 +0800

    properly chosen font and color

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "@fontsource/instrument-sans/latin-400.css";
-import "@fontsource/instrument-sans/latin-500.css";
-import "@fontsource/instrument-sans/latin-600.css";
-import "@fontsource/inter/latin-400.css";
-import "@fontsource/inter/latin-500.css";
+import "./styles/fonts.css";
 import "./styles/variables.css";
 import "./styles/globals.css";
 import "./styles/typography.css";
